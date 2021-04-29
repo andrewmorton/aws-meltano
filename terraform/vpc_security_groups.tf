2546bcc76e06c004b3add32b4fa973b2366c1333
Andrew Morton <ajmortonit@gmail.com>
2021-04-29 12:35:38 -0400

    Allow for postgresql traffic from sysadmin

--- terraform/vpc_security_groups.tf
+++ terraform/vpc_security_groups.tf
@@ -26,12 +26,13 @@ resource aws_security_group "meltano_rds_sg" {
   vpc_id = data.aws_vpc.deployment_vpc.id
   
   ingress {
-    description = "Postgresql traffic from eks cluster group"
+    description = "Postgresql traffic from eks cluster group and sysadmin"
     from_port = 5432
     to_port = 5432
     protocol = "TCP"
     security_groups = [
-      module.eks_cluster.cluster_primary_security_group_id
+      module.eks_cluster.cluster_primary_security_group_id,
+      aws_security_group.sysadmin_security_group.id
     ]
   }
   
