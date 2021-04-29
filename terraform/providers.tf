f992d14d792d43ba1538769bcf8035abc19f444a
Andrew Morton <ajmortonit@gmail.com>
2021-04-29 09:20:17 -0400

    First commit

---
+++ terraform/providers.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "us-east-1"
+  version = "3.25.0"
+  assume_role {
+    role_arn = var.source_provider_assume_role_arn
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket = "mavencollective-net-tf"
+    region = "us-east-1"
+    key = "mavencollective-iam-service"
+  }
+}
